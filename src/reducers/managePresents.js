985a68e7f81683d65ac3481cdf21745ee0ed529b
Desiree Lerma <desi.lerma96@yahoo.com>
2021-03-09 22:47:36 -0600

    commit tree 4df52e5ca7984f2e9d93e243955d64a9adc060e2

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,15 @@
 export function managePresents(state, action){
+    //set default state to 0
+    state.numberOfPresents = 0
+    // use a switch statement for conditional logic for our action
+    switch(action.type) {
+        // using action.type allows us to pass in conditional info
+        // allows for more flexibility with arg
+        // can pass in obj instead of string
+        //string wont allow for lots of customization or changes
+        case 'INCREASE' :
+            return {numberOfPresents: state.numberOfPresents + 1}
+        default:
+            return state;
+    }
 }
